8b5b8d97411772292e0a9c30fe8245ad93a9a792
hitoshi.hasumi <hitoshi.hasumi@monstar-lab.com>
2014-10-08 18:26:25 +0900

    close #5

--- client/lib/main.rb
+++ client/lib/main.rb
@@ -4,6 +4,36 @@ require 'pry'
 
 class Main
 
+  COUNTERS =
+   [[' 3333333 ',
+     '33     33',
+     '      33 ',
+     '    333  ',
+     '      33 ',
+     '33     33',
+     ' 3333333 '],
+    [' 2222222 ',
+     '22     22',
+     '      22 ',
+     '    22   ',
+     '  22     ',
+     ' 22      ',
+     '222222222'],
+    ['   111   ',
+     '  1111   ',
+     '    11   ',
+     '    11   ',
+     '    11   ',
+     '    11   ',
+     ' 11111111'],
+    ['         ',
+     '         ',
+     ' G A M E ',
+     '         ',
+     'S T A R T',
+     '         ',
+     '         ']]
+
   def initialize(host='localhost')
     Curses.init_screen
     Curses.cbreak
@@ -12,6 +42,7 @@ class Main
     @field = Field.new
     @sock = TCPSocket.open(host, 12345)
     flush
+    count_down
     game
   end
 
@@ -37,6 +68,22 @@ class Main
   def flush
     @field.set_pos JSON.parse(@sock.gets).deep_symbolize_keys
     @field.flush
+  end
+
+  # @author hasumi
+  # ゲーム開始のカウントダウン
+  def count_down
+    COUNTERS.each do |counter|
+      Curses.setpos((Field::HEIGHT / 2).floor - (COUNTERS[0].size / 2).floor + 4, 0)
+      counter.each do |split_counter|
+        line = Field::INFIELD.dup
+        pos = (Field::WIDTH / 2).floor - (split_counter.size / 2).floor
+        line[(pos)..(pos + split_counter.size - 1)] = split_counter
+        Curses.addstr "#{line}\n"
+      end
+      Curses.refresh
+      sleep 1
+    end
   end
 
 end
